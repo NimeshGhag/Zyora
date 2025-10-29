fcb9768eda63aaa05e030b105166c75f2dc7500a
NimeshGhag <ghagnimesh490@gmail.com>
2025-10-29 15:57:33 +0530

    project completed

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -1,9 +1,10 @@
+import { lazy } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
+const App = lazy(() => import("./App.jsx"));
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from 'react-redux'
-import { store } from './app/store';
+import { Provider } from "react-redux";
+import { store } from "./app/store";
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
